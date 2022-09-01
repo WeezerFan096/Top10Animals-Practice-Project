01d0c01aa94aafdffd49f0887631a90798a58472
dakotarobot213 <110577432+dakotarobot213@users.noreply.github.com>
2022-08-31 22:31:55 -0700

    Added JS Enhanced page recommendations
    Added a JavaScript powered 'prompt' to the bottom of the main page to point the user to a random animal page.

--- functions.js
+++ functions.js
@@ -13,12 +13,18 @@ const animalList = [
 
 //Randomly generates a positive whole number within a given range
 function intRandomBy(range) {
-	return parseInt(Math.random() * (range + 1) + 0.5);
+	return parseInt(Math.random() * range);
 }
 
 //Randomly picks an animal from const array 'animalList' and returns the animal name
 function animalSuggest() {
-	return animalList[intRandomBy(animalList.length())];
+	return animalList[intRandomBy(animalList.length)];
 }
 
-animalSuggest();
+//Called on site boot to run necessary first boot functions and commands
+function siteLoad() {
+	//Sets any html element with the CSS id 'randomAnimalSuggest' to 'Maybe check out the (insert_animal_name)'s page?'
+	document.getElementById(
+		'randomAnimalSuggest'
+	).innerHTML = `Maybe check out the ${animalSuggest()}'s page?`;
+}
